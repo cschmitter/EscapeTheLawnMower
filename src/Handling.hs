0789e9c5602072ae679b49f3bf8de6158b258c2e
Cameron Schmitter <kinggemit@users.noreply.github.com>
2020-05-07 15:33:29 -0700

    changed keys to direct handling

--- src/Handling.hs
+++ src/Handling.hs
@@ -1,13 +1,59 @@
 module Handling where
 
 import World
-import qualified Data.Set as S
 
 import Graphics.Gloss.Interface.IO.Game
 
 handleInput :: Event -> World -> World
-handleInput (EventKey k Down _ _) w = w {keys = S.insert k (keys w)}
-handleInput (EventKey k Up _ _) w = w {keys = S.delete k (keys w)}
+handleInput (EventKey k Down _ _) w
+  | elem k keyGoLeft = playerDo GoLeft w
+  | elem k keyGoRight = playerDo GoRight w
+  | elem k keyGoJump = playerDo GoJump w
+  | elem k keyGoFlip = playerDo GoFlip w
+  
+handleInput (EventKey k Up _ _) w
+  | elem k keyGoLeft = playerDo GoRight w
+  | elem k keyGoRight = playerDo GoLeft w
+
 handleInput (EventMotion pos) w = w {mouse = pos}
+
 handleInput _ w = w
 
+---------------------------------------------
+-- Key Definitions
+---------------------------------------------
+
+keyGoLeft :: [Key]
+keyGoLeft = [Char 'h', Char 'a', SpecialKey KeyLeft]
+  
+keyGoRight :: [Key]
+keyGoRight = [Char 'l', Char 'd', SpecialKey KeyRight]
+  
+keyGoJump :: [Key]
+keyGoJump = [Char 'k', Char 'w', SpecialKey KeyUp, SpecialKey KeySpace]
+  
+keyGoFlip :: [Key]
+keyGoFlip = [Char 'f']
+
+---------------------------------------------
+-- Key Actions
+---------------------------------------------
+
+playerDo :: Action -> World -> World
+playerDo action w = w { player = newPlayer }
+  where p = player w
+        (oldvX, oldvY) = playerVelocity p
+        (oldaX, oldaY) = playerAcceleration p
+        
+        newPlayer
+          | elem action [GoLeft, GoRight, GoJump] = p { playerVelocity = newVelocity }
+          | action == GoFlip                      = p { playerAcceleration = newAccel }
+          | otherwise                             = p
+          
+        newVelocity = case action of GoLeft -> (oldvX - playerSpeed p, oldvY)
+                                     GoRight -> (oldvX + (playerSpeed p), oldvY)
+                                     GoJump -> (oldvX, playerJump p)
+                                     _otherwise -> playerVelocity p
+
+        newAccel = case action of GoFlip -> (oldaX, -oldaY)
+                                  _otherwise -> playerAcceleration p
